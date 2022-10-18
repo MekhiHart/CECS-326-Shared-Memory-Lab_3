0d28e5e4705b17ac7d7a2f291a7c6622adef5718
Mekhi <mekhihartdelacruz@gmail.com>
2022-10-18 13:59:23 -0700

    successfully wrote on slave to shared memory. Need to figure out how to increment struct index with race condition

--- master.c
+++ master.c
@@ -20,6 +20,11 @@
 #include <errno.h>
 using namespace std;
 
+struct CLASS { // struct shared memory implementation
+	int index;			// index to next available response slot
+	int response[10];	// each child writes its child number here
+};
+
 
 int main(int argc, char** argv) {
 	pid_t cpid; // fork id
@@ -27,13 +32,8 @@ int main(int argc, char** argv) {
 	int num_children = atoi(argv[1]); // takes argv[1] from char** to int
 	const int SIZE = 4096;
 	const char *name = argv[2]; // shm name
-	char *shm_base; // base address of base memory
-	char *shm_ptr; // moveable ptr
-	struct CLASS {
-		int index;			// index to next available response slot
-		int response[10];	// each child writes its child number here
-	};
-	// struct is what you write into the shared memory
+	struct CLASS  *shm_base; // base address of base memory
+	struct CLASS *shm_ptr; // moveable ptr
 
 	printf("Master begins execution\n");
 	printf("This is %d\n", getpid());
@@ -46,25 +46,36 @@ int main(int argc, char** argv) {
 	}
 
 	ftruncate(shm_fd, SIZE); // configures size of the shared memory object
-	shm_base = (char *)  mmap(0,SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd,0); // associates memory segment into return pointer
+	shm_base = (struct CLASS *)  mmap(0,SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd,0); // associates memory segment into return pointer
 	if (shm_base == MAP_FAILED){
 		printf("Map failed");
 		exit(1);
 	}
-	
+	shm_base->index=-1;
+	//shm_ptr = shm_base;
+//	printf("Index pointer: %d\n",shm_ptr->index);
+//	shm_ptr->index += 1;
+//	printf("Base pointer: %d\n",shm_base->index);
+	 // starts at -1 for it to increment before exec; -1 -> 0
 	
 	for (int i=0; i < num_children; i++){ //creating child processes
+		shm_base->index += 1;
 		cpid = fork();
-		char childNum[5];
+		char childNum[5]; //buffer for second argument in execlp()
 		sprintf(childNum,"%d",i + 1);
 		if (cpid == 0){ // child process
+			//shm_base->index += 1;
+			
 			execlp("./slave",name,childNum,NULL);
-
 		}
+		//printf("Inside master, index is : %d\n",shm_base->index);
+
+		//shm_base->index += 1;
+
 	}
 	while(wait(NULL) != -1); // blocks code until all children have terminated
 	printf("Master received termination signals from all %d child processes",num_children);
-	
+	printf("Shared memory first index: %d\n",shm_base->response[0]);	
 	shm_unlink(name); // unlinks shared memory
 	exit(0); // terminates program
 }
--- slave.c
+++ slave.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+	#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
@@ -9,38 +9,37 @@
 #include <string.h>
 using namespace std;
 
+struct CLASS {
+	int index;			// index to next available response slot
+	int response[10];	// each child writes its child number here
+};// how to implement struct into this??
+
 int main(int argc, char **argv){
 	// first argument: shm name
 	// second argument: child idx
 	// third argument: size
-	struct CLASS {
-		int index;			// index to next available response slot
-		int response[10];	// each child writes its child number here
-	};// how to implement struct into this??
 
 	int shm_fd;
-	char *shm_base;
-	char *shm_ptr;
+	struct CLASS *shm_base;
+	struct CLASS *shm_ptr;
 	printf("Slave begins execution\n");
 	printf("I am child number %s, received shared memory name %s\n",argv[1],argv[0]);
 
-	shm_fd = shm_open(argv[0], O_RDONLY, 0666);
+	shm_fd = shm_open(argv[0], O_RDWR, 0666);
 	if (shm_fd == -1) {// Shared memory failed
 		printf("cons: Shared memory failed: %s\n", strerror(errno));
 	 	exit(1);
 	}
-	shm_base = (char *) mmap(0, 4096, PROT_READ, MAP_SHARED, shm_fd, 0);
+	shm_base = (struct CLASS *) mmap(0, 4096, PROT_WRITE, MAP_SHARED, shm_fd, 0);
 	if (shm_base == MAP_FAILED) { // map failed
 		printf("cons: Map failed: %s\n", strerror(errno));
  		/* close and unlink */
 		exit(1);
  	} 
-	shm_ptr = shm_base;
+	printf("I am child number %s, and the next empty slot is %d\n",argv[1],shm_base->index);
 	// writes into shared memory
-	const char *test="hello";
-	sprintf(shm_ptr,"%s",test); // error here
-	printf("adds into shm_ptr of test content\n");
-	shm_ptr += strlen(test);
+//	shm_base->index += 1; // increases next index
+	shm_base->response[0] = 420; // test
 	printf("I have written my child number to shared memory\n");
 	
 	shm_unlink(argv[0]); // unlinks shm_memory
