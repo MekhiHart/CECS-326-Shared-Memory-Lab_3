fd59de2680c1d077bd5e2e05c3f4c0c334d80396
Mekhi <mekhihartdelacruz@gmail.com>
2022-10-18 19:17:36 -0700

    Refractored files; produces right results

--- master.c
+++ master.c
@@ -22,19 +22,19 @@
 //using namespace std;
 
 struct CLASS { // struct shared memory implementation
-	int index;			// index to next available response slot
 	int response[10];	// each child writes its child number here
 };
 
+void display(struct CLASS *base, int numChildren);
 
 int main(int argc, char** argv) {
 	pid_t cpid; // fork id
-	int shm_fd;
+	int shm_fd; // shared memory file descriptor 
 	int num_children = atoi(argv[1]); // takes argv[1] from char** to int
 	const int SIZE = 4096;
 	const char *name = argv[2]; // shm name
 	struct CLASS  *shm_base; // base address of base memory
-	struct CLASS *shm_ptr; // moveable ptr
+	//struct CLASS *shm_ptr; // moveable ptr
 
 	printf("Master begins execution\n");
 	printf("This is %d\n", getpid());
@@ -45,55 +45,44 @@ int main(int argc, char** argv) {
 		printf("Shared memory failed\n");
 		exit(1);
 	}
-
 	ftruncate(shm_fd, SIZE); // configures size of the shared memory object
 	shm_base = (struct CLASS *)  mmap(0,SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd,0); // associates memory segment into return pointer
 	if (shm_base == MAP_FAILED){
 		printf("Map failed");
 		exit(1);
 	}
-	shm_base->index=-1;
-	//shm_ptr = shm_base;
-//	printf("Index pointer: %d\n",shm_ptr->index);
-//	shm_ptr->index += 1;
-//	printf("Base pointer: %d\n",shm_base->index);
-	 // starts at -1 for it to increment before exec; -1 -> 0
-	
-	for (int i=0; i < num_children; i++){ //creating child processes
-		//shm_base->index += 1;
-		cpid = fork();
+	for (int i=0; i < num_children; i++){ //creates multiple child processes depending on childNumber
+
+		cpid = fork(); // creates child process
 		char childNum[5]; //buffer for second argument in execlp()
 		sprintf(childNum,"%d",i + 1);
 		if (cpid == 0){ // child process
-			//shm_base->index += 1;
 
 			execlp("./slave",name,childNum,NULL);
 		}
-		//printf("Inside master, index is : %d\n",shm_base->index);
-
-		//shm_base->index += 1;
-
 	}
-	while(wait(NULL) != -1); // blocks code until all children have terminated
+	while(wait(NULL) != -1); // blocks code after it until all children have terminated
 	printf("Master received termination signals from all %d child processes\n",num_children);
-	printf("Shared memory first index: %d\n",shm_base->response[0]);
-	printf("Shared memory second index: %d\n",shm_base->response[1]);
-	//printf("Shared memory third index: %d\n",shm_base->response[2]);	
-	// removes shared memory
+	printf("Contents of shared memory segment filled by child processes:\n");
+	display(shm_base,num_children);
 
+	// Removes shared memory
 	if (munmap(shm_base, SIZE) == -1) { // removes mapped memory segment
  		printf("prod: Unmap failed: %s\n", strerror(errno));
  		exit(1);
 	}
 
-
-
 	if (close(shm_fd) == -1) { // closes shared memory segment
- 		printf("prod: Close failed: %s\n",
-		strerror(errno));
-		exit(1);
+ 		printf("prod: Close failed: %s\n", strerror(errno));
+		exit(1); // terminates program
 	 }
 
-	//shm_unlink(name); // unlinks shared memory
 	exit(0); // terminates program
+}
+
+void display(struct CLASS *base, int numChildren){
+	for (int i =0; i < numChildren; i++){
+		printf("%d ", base->response[i]);
+	}
+	printf("\n");
 }
--- slave.c
+++ slave.c
@@ -10,39 +10,34 @@
 //using namespace std;
 
 struct CLASS {
-	int index;			// index to next available response slot
 	int response[10];	// each child writes its child number here
 };// how to implement struct into this??
 
 int main(int argc, char **argv){
-	// first argument: shm name
-	// second argument: child idx
-	// third argument: size
 	const int SIZE = 4096;
 	int shm_fd;
 	struct CLASS *shm_base;
-	struct CLASS *shm_ptr;
 	int arrIdx = atoi(argv[1]); // index needed to change;
 	printf("Slave begins execution\n");
 	printf("I am child number %s, received shared memory name %s\n",argv[1],argv[0]);
 
-	shm_fd = shm_open(argv[0], O_RDWR, 0666);
+	shm_fd = shm_open(argv[0], O_RDWR, 0666); // opens shared memory file assoicated with the name given
 	if (shm_fd == -1) {// Shared memory failed
 		printf("cons: Shared memory failed: %s\n", strerror(errno));
 	 	exit(1);
 	}
-	shm_base = (struct CLASS *) mmap(0, SIZE,PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+	shm_base = (struct CLASS *) mmap(0, SIZE,PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0); // returns a memory address of the assoicated file descriptor passed in with the instructions to write on it
 	if (shm_base == MAP_FAILED) { // map failed
 		printf("cons: Map failed: %s\n", strerror(errno));
  		/* close and unlink */
-		exit(1);
- 	} 
-	//printf("I am child number %s, and the next empty slot is %d\n",argv[1],shm_base->index);
+		exit(1); // terminates program
+ 	}
+
 	// writes into shared memory
 	printf("Value being written into child num %s: %d",argv[1],arrIdx);
 	shm_base->response[arrIdx - 1] = arrIdx; // test successful
 	printf("I have written my child number to shared memory\n");
-	
+
 	// removes shared memory segment
 	if (munmap(shm_base, SIZE) == -1) { // removes memory mapping 
  		printf("prod: Unmap failed: %s\n", strerror(errno));
@@ -54,7 +49,6 @@ int main(int argc, char **argv){
  		exit(1);
 	 }
 
-	//shm_unlink(argv[0]); // unlinks shm_memory
 	printf("Slave closed access to shared memory and terminated\n");
-	exit(1);
+	exit(1); // terminates program
 }
